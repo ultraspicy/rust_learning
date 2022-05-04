3aa569940a2681397e60f63b2e49a11aa0b29d77
ultraspicy <73217673+ultraspicy@users.noreply.github.com>
2022-05-03 18:33:12 -0700

    Add lifetimes learning notes

--- generic_traits_lifetimes/target/debug/build/num-traits-f3a27c1158b73d84/build_script_build-f3a27c1158b73d84.d
+++
@@ -1,5 +0,0 @@
-/Users/jianfeng/Developer/rust_learning/generic_traits_lifetimes/target/debug/build/num-traits-f3a27c1158b73d84/build_script_build-f3a27c1158b73d84: /Users/jianfeng/.cargo/registry/src/github.com-1ecc6299db9ec823/num-traits-0.2.15/build.rs
-
-/Users/jianfeng/Developer/rust_learning/generic_traits_lifetimes/target/debug/build/num-traits-f3a27c1158b73d84/build_script_build-f3a27c1158b73d84.d: /Users/jianfeng/.cargo/registry/src/github.com-1ecc6299db9ec823/num-traits-0.2.15/build.rs
-
-/Users/jianfeng/.cargo/registry/src/github.com-1ecc6299db9ec823/num-traits-0.2.15/build.rs:
--- generic_traits_lifetimes/target/debug/deps/generic_traits_lifetimes-b70594cc370f339e.d
+++
@@ -1,5 +0,0 @@
-/Users/jianfeng/Developer/rust_learning/generic_traits_lifetimes/target/debug/deps/generic_traits_lifetimes-b70594cc370f339e: src/main.rs
-
-/Users/jianfeng/Developer/rust_learning/generic_traits_lifetimes/target/debug/deps/generic_traits_lifetimes-b70594cc370f339e.d: src/main.rs
-
-src/main.rs:
