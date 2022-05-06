b8f9a329a20ea87e5b726355e621cb3263332823
ultraspicy <73217673+ultraspicy@users.noreply.github.com>
2022-05-06 15:02:50 -0700

    Minor change

--- test_in_rust/target/debug/deps/integration_test-46e8a95fa965ac4b.d
+++
@@ -1,5 +0,0 @@
-/Users/jianfeng/Developer/rust_learning/test_in_rust/target/debug/deps/integration_test-46e8a95fa965ac4b: tests/integration_test.rs
-
-/Users/jianfeng/Developer/rust_learning/test_in_rust/target/debug/deps/integration_test-46e8a95fa965ac4b.d: tests/integration_test.rs
-
-tests/integration_test.rs:
--- test_in_rust/target/debug/deps/test_in_rust-b5569eb46411260a.d
+++
@@ -1,7 +0,0 @@
-/Users/jianfeng/Developer/rust_learning/test_in_rust/target/debug/deps/test_in_rust-b5569eb46411260a.rmeta: src/lib.rs
-
-/Users/jianfeng/Developer/rust_learning/test_in_rust/target/debug/deps/libtest_in_rust-b5569eb46411260a.rlib: src/lib.rs
-
-/Users/jianfeng/Developer/rust_learning/test_in_rust/target/debug/deps/test_in_rust-b5569eb46411260a.d: src/lib.rs
-
-src/lib.rs:
