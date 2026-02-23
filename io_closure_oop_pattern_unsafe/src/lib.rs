5c0f1281778f2fcebd2bf5710dd5d39b3df92117
Jianfeng Guo <element.no.88@gmail.com>
2026-02-23 09:49:50 -0800

    Add state pattern (OOP discussion)

--- io_closure_oop_pattern_unsafe/src/lib.rs
+++ io_closure_oop_pattern_unsafe/src/lib.rs
@@ -140,3 +140,36 @@ pub fn build_ui() {
 }
 
 // state pattern
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Self {
+        Post { 
+            state: Some(Box::new(Draft{})),
+            content: String::new(), 
+        }
+    }
+
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn content(&self) -> &str {
+        ""
+    }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            
+        }
+    }
+}
+
+trait State {}
+
+struct Draft{}
+
+impl State for Draft {}
