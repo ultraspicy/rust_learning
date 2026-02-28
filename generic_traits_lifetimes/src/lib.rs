f2cc9ce276a7c95379108431a71cb89d18240925
Jianfeng Guo <element.no.88@gmail.com>
2026-02-27 20:59:19 -0800

    Add macros

--- generic_traits_lifetimes/src/lib.rs
+++ generic_traits_lifetimes/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod basics;
 pub mod advanced;
 pub mod newtype_and_alias;
+pub mod macros;
---
+++ generic_traits_lifetimes/src/macros.rs
@@ -0,0 +1,6 @@
+// declarative macros with macro_rules! and three kinds of procedural macros:
+// Custom #[derive] macros
+// Attribute-like macros
+// Function-like macros
+
+// Macros run at compile time, before the compiler interprets the code.
